3d47fa75cf546a001b65138377f34059dbfa8d90
jagadeesh.bevara <jagadeesh.bevara>
2023-02-10 19:17:44 +0530

    updated clientid

--- src/launch.jsx
+++ src/launch.jsx
@@ -5,7 +5,7 @@ import FHIR from 'fhirclient';
 function Launch () {
     <script src="https://cdn.jsdelivr.net/npm/fhirclient/build/fhir-client.js"></script>
     FHIR.oauth2.authorize({
-      clientId: "f5ff1bd2-34e6-4774-a03e-3a989b20bf91",
+      clientId: "49404fbc-de2c-4030-bfc3-207fde3226e2",
       scope: "patient/Patient.read patient/Observation.read launch online_access openid profile",
       iss:"https://fhir-ehr-code.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d",
       redirectUri: "http://10.10.13.14:3444/patientDetails",
